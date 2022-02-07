9c3ae240854bebf8179e5a28bb23556a295445cb
prasad rajalingamgari <32452178+prasadrvsv@users.noreply.github.com>
2022-02-06 21:58:41 -0800

    Updating Swift Clinet (#1)
    Co-authored-by: Prasad Rajalingamgari <prasadrajalingamgari@Prasads-MacBook-Pro.local>

--- MyLibrary/Tests/MyLibraryTests/MyLibraryTests.swift
+++ MyLibrary/Tests/MyLibraryTests/MyLibraryTests.swift
@@ -2,115 +2,41 @@ import XCTest
 import MyLibrary
 
 final class MyLibraryTests: XCTestCase {
-    func testIsLuckyBecauseWeAlreadyHaveLuckyNumber() throws {
+    func testHello() throws {
         // Given
-        let mockWeatherService = MockWeatherService(
-            shouldSucceed: true,
-            shouldReturnTemperatureWithAnEight: false
-        )
-
-        let myLibrary = MyLibrary(weatherService: mockWeatherService)
-        let number = 8
-
-        let expectation = XCTestExpectation(description: "We asked about the number 8 and heard back 🎄")
-        var isLuckyNumber: Bool?
-        print("One")
+        let myLibrary = MyLibrary()
+        let expectation = XCTestExpectation(description: "Ping the welcome page but heard back 🎄")
+        var msgReceived: Bool?
 
         // When
-        myLibrary.isLucky(number, completion: { lucky in
-            print("Two")
-            isLuckyNumber = lucky
+        myLibrary.isMsgReceived(completion: { lucky in
+            msgReceived = lucky
             expectation.fulfill()
         })
 
-        print("Three")
-
         wait(for: [expectation], timeout: 5)
 
-        print("Four")
-
         // Then
-        XCTAssertNotNil(isLuckyNumber)
-        XCTAssert(isLuckyNumber == false)
+        XCTAssertNotNil(msgReceived)
+        XCTAssert(msgReceived == true)
     }
 
-    func testIsLuckyBecauseWeatherHasAnEight() throws {
+    func testWeather() throws {
         // Given
-        let mockWeatherService = MockWeatherService(
-            shouldSucceed: true,
-            shouldReturnTemperatureWithAnEight: true
-        )
-
-        let myLibrary = MyLibrary(weatherService: mockWeatherService)
+        let myLibrary = MyLibrary()
         let number = 0
         let expectation = XCTestExpectation(description: "We asked about the number 8 and heard back 🎄")
         var isLuckyNumber: Bool?
-        print("Five")
-        
 
         // When
         myLibrary.isLucky(number, completion: { lucky in
-            print("Six")
             isLuckyNumber = lucky
             expectation.fulfill()
         })
-        print("Seven")
 
         wait(for: [expectation], timeout: 5)
-        print("Eight")
 
         // Then
         XCTAssertNotNil(isLuckyNumber)
         XCTAssert(isLuckyNumber == true)
     }
-
-    func testIsNotLucky() throws {
-        // Given
-        let mockWeatherService = MockWeatherService(
-            shouldSucceed: true,
-            shouldReturnTemperatureWithAnEight: false
-        )
-
-        let myLibrary = MyLibrary(weatherService: mockWeatherService)
-        let number = 7
-        let expectation = XCTestExpectation(description: "We asked about the number 7 and heard back 🌲")
-        var isLuckyNumber: Bool?
-
-        // When
-        myLibrary.isLucky(number, completion: { lucky in
-            isLuckyNumber = lucky
-            expectation.fulfill()
-        })
-
-        wait(for: [expectation], timeout: 5)
-
-        // Then
-        XCTAssertNotNil(isLuckyNumber)
-        XCTAssert(isLuckyNumber == false)
-    }
-
-    func testIsNotLuckyBecauseServiceCallFails() throws {
-        // Given
-        let mockWeatherService = MockWeatherService(
-            shouldSucceed: false,
-            shouldReturnTemperatureWithAnEight: false
-        )
-
-        let myLibrary = MyLibrary(weatherService: mockWeatherService)
-        let number = 7
-        let expectation = XCTestExpectation(description: "We asked about the number 7 but the service call failed 🤖💩")
-        var isLuckyNumber: Bool?
-
-        // When
-        myLibrary.isLucky(number, completion: { lucky in
-            isLuckyNumber = lucky
-            expectation.fulfill()
-        })
-
-        wait(for: [expectation], timeout: 5)
-
-        // Then
-        XCTAssertNil(isLuckyNumber)
-    }
-
-}
